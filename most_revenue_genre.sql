f4db339acb9a743abfbc8908f473c63d97607a1a
Sina Sartip <ssartipz@ryerson.ca>
2021-04-01 18:07:53 -0400

    genre revenue and album revenue added

--- most_revenue_genre.sql
+++ most_revenue_genre.sql
@@ -1,10 +1,10 @@
 -- SQLite
 /* Which track generated the most revenue? which album? which genre?
  *  1- The invoice_item and tracks table keep track of sales and name of songs
- *  use a join to have the fina track total with names. 
+ *  use a join to have the final track total with names. 
  *  2- The track_sales can now be joined with the tracks table 
- *  and then the album table.
- *  3- Two albums A and B. Which generated the most revenue?
+ *  and then the genre table.
+ *  3- Two genre A and B. Which generated the most revenue?
  *  A -> had 7 songs sold from it and B-> had 30 songs.
  *  Is this enough information?
  *  Are all song proces equal-> This could make the 7 songs worth more.
@@ -26,25 +26,25 @@ track_sales AS(
     GROUP BY track_invoice.Name
     ORDER BY quantity_sold DESC
 ),
-album_sales AS(
-SELECT tracks.AlbumId,
-    COUNT(track_sales.quantity_sold) AS numOf_songSale_inAlbum,
+ungrouped_genre_sales AS(
+SELECT tracks.GenreId,
+    COUNT(track_sales.quantity_sold) AS numOf_songSale_inGenre,
     SUM(tracks.UnitPrice) AS revenue_from_sale
 FROM track_sales
 JOIN tracks
     ON tracks.Name = track_sales.Name 
 GROUP BY track_sales.Name
-ORDER BY AlbumId DESC
+ORDER BY GenreId DESC
 )
 
-SELECT albums.Title AS album,
-    SUM(album_sales.numOf_songSale_inAlbum) AS songs_from_album_sold,
-    SUM(album_sales.revenue_from_sale) AS revenue_from_album
-FROM album_sales
-JOIN albums
-    ON albums.AlbumId = album_sales.AlbumId
-GROUP BY albums.Title
-ORDER BY revenue_from_album DESC;
+SELECT genres.Name AS genre,
+    SUM(ungrouped_genre_sales.numOf_songSale_inGenre) AS songs_from_genre_sold,
+    SUM(ungrouped_genre_sales.revenue_from_sale) AS revenue_from_genre
+FROM ungrouped_genre_sales
+JOIN genres
+    ON genres.GenreId = ungrouped_genre_sales.GenreId
+GROUP BY genres.Name
+ORDER BY revenue_from_genre DESC;
 
 
 
